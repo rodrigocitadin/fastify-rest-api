593f80e227f49a089f2ca5872e18d6868c6a7234
rodrigocitadin <citadinrodrigo@gmail.com>
2023-02-20 14:31:20 -0300

    fix: switch ps to sqlite

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,15 @@
-import { knex as knexSetup } from 'knex';
+import { Knex, knex as knexSetup } from 'knex';
 
-export const knex = knexSetup({
-  client: 'pg',
+export const config: Knex.Config = {
+  client: 'sqlite',
   connection: {
-    host: 'localhost',
-    user: 'postgres',
-    password: 'postgres',
-    database: 'fastify_rest_api'
+    filename: './priv/db/app.db'
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './priv/migrations'
   }
-});
+};
+
+export const knex = knexSetup(config);
