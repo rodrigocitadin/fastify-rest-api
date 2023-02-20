e3f9b6148535a82c9a1c7af0de3ca549c14fe43c
rodrigocitadin <citadinrodrigo@gmail.com>
2023-02-20 14:55:37 -0300

    feat: create random things to test knex

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,27 @@
 import fastify from "fastify";
+import crypto from "node:crypto";
+import { knex } from "./database";
 
 const app = fastify();
 
 app.listen({ port: 3333 })
-  .then(() => console.log('Server is running!'))
+  .then(() => console.log('Server is running!'));
+
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Test transaction',
+      amount: 1000
+    })
+    .returning('*');
+
+  return transaction;
+})
+
+app.get('/bye', async () => {
+  const transaction = await knex('transactions')
+    .select('*');
+
+  return transaction;
+})
