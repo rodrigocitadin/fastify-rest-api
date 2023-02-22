40f2e55b3f4d8e0a1a780a8e72f298180bbd9cea
rodrigocitadin <citadinrodrigo@gmail.com>
2023-02-22 01:36:13 -0300

    feat: get all or unique transaction by id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,18 +4,37 @@ import { z } from "zod";
 import { knex } from "../database";
 
 export async function transactionsRoutes(app: FastifyInstance) {
+
+    app.get('/', async () => {
+        const transactions = await knex('transactions').select();
+
+        return { transactions };
+    });
+
+    app.get('/:id', async (request) => {
+        const getTransactionsParamsSchema = z.object({
+            id: z.string().uuid()
+        });
+
+        const { id } = getTransactionsParamsSchema.parse(request.params);
+
+        const transactions = await knex('transactions').where('id', id).first();
+
+        return { transactions };
+    });
+
     app.post('/', async (request, reply) => {
         const createTransactionBodySchema = z.object({
-            tittle: z.string(),
+            title: z.string(),
             amount: z.number(),
             type: z.enum(['credit', 'debit']),
         });
 
-        const { tittle, amount, type } = createTransactionBodySchema.parse(request.body);
+        const { title, amount, type } = createTransactionBodySchema.parse(request.body);
 
         await knex('transactions').insert({
             id: randomUUID(),
-            tittle,
+            title,
             amount: type === 'credit' ? amount : amount * -1,
         });
 
